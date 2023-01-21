e930c6a56ab8197bbfad569c9b175c2917e430d2
eion <rolvinjm@gmail.com>
2023-01-21 15:04:30 +0530

    implemented security authorization

--- src/main/java/com/eion/restapi/Security/CustomUserDetailsService.java
+++ src/main/java/com/eion/restapi/Security/CustomUserDetailsService.java
@@ -1,12 +1,35 @@
 package com.eion.restapi.Security;
 
+import com.eion.restapi.entity.User;
+import com.eion.restapi.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class CustomUserDetailsService  implements UserDetailsService {
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with the user name or email" + usernameOrEmail));
+
+        Set<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                authorities);
     }
 }
