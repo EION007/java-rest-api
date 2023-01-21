8caf66d8faabe46ee6822001304ef773ba25dc23
eion <rolvinjm@gmail.com>
2023-01-21 14:47:02 +0530

    updated code

--- src/main/java/com/eion/restapi/controller/PostController.java
+++ src/main/java/com/eion/restapi/controller/PostController.java
@@ -20,19 +20,13 @@ public class PostController {
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {
-        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
-    }
+    public ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);}
 
     @GetMapping
-    public ResponseEntity<PostResponse> getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE ,required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir
-    ) {
-        return new ResponseEntity<>(postService.getAllPost(pageNo,pageSize,sortBy,sortDir), HttpStatus.OK);
+    public ResponseEntity<PostResponse> getAllPosts(@RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo, @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize, @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy, @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
+        return new ResponseEntity<>(postService.getAllPost(pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
     }
+
     @GetMapping("{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
         return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
@@ -49,6 +43,6 @@ public class PostController {
     @DeleteMapping("{id}")
     public ResponseEntity<String> deletePost(@PathVariable(value = "id") Long id) {
         postService.deletePost(id);
-        return new ResponseEntity<>("post with :"+id+" Deleted",HttpStatus.OK);
+        return new ResponseEntity<>("post with :" + id + " Deleted", HttpStatus.OK);
     }
 }
