b8a9861c481491ebb0ba6d4f1f364315764ceeff
EION <102137886+EION007@users.noreply.github.com>
2023-01-21 14:48:59 +0530

    Feature a (#11)
    * added role repo
    
    * updated code

--- src/main/java/com/eion/restapi/controller/PostController.java
+++ src/main/java/com/eion/restapi/controller/PostController.java
@@ -19,29 +19,14 @@ public class PostController {
     private PostService postService;
 
     @PostMapping
-
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {
-
-
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {
-
-    private ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {
-
-
-        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
-    }
+    public ResponseEntity<PostDto> createPosts(@Valid @RequestBody PostDto postDto) {return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);}
 
     @GetMapping
-    public ResponseEntity<PostResponse> getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE ,required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir
-    ) {
-        return new ResponseEntity<>(postService.getAllPost(pageNo,pageSize,sortBy,sortDir), HttpStatus.OK);
+    public ResponseEntity<PostResponse> getAllPosts(@RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo, @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize, @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy, @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
+        return new ResponseEntity<>(postService.getAllPost(pageNo, pageSize, sortBy, sortDir), HttpStatus.OK);
     }
+
     @GetMapping("{id}")
     public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
         return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
@@ -53,10 +38,11 @@ public class PostController {
         return new ResponseEntity<>(postService.updatePost(postDto, id), HttpStatus.OK);
     }
 
+
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deletePost(@PathVariable(value = "id") Long id) {
         postService.deletePost(id);
-        return new ResponseEntity<>("post with :"+id+" Deleted",HttpStatus.OK);
+        return new ResponseEntity<>("post with :" + id + " Deleted", HttpStatus.OK);
     }
 }
