9974933273867fa27107a74205278d343483ebc6
eion <rolvinjm@gmail.com>
2023-01-22 22:23:32 +0530

    adding code v.8

--- src/main/java/com/eion/restapi/RestapiApplication.java
+++ src/main/java/com/eion/restapi/RestapiApplication.java
@@ -1,16 +1,13 @@
 package com.eion.restapi;
 
-import com.eion.restapi.entity.Role;
-import com.eion.restapi.repository.RoleRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class RestapiApplication implements CommandLineRunner {
+//implements CommandLineRunner
+public class RestapiApplication {
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -21,13 +18,16 @@ public class RestapiApplication implements CommandLineRunner {
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
-	@Autowired
-	private RoleRepository roleRepository;
-	@Override
-	public void run(String... args) throws Exception {
-		Role adminRole = new Role();
-		adminRole.setName("ROLE_ADMIN");
-		roleRepository.save(adminRole);
-
-	}
+//	@Autowired
+//	private RoleRepository roleRepository;
+//	@Override
+//	public void run(String... args) throws Exception {
+//		Role adminRole = new Role();
+//		adminRole.setName("ROLE_ADMIN");
+//		roleRepository.save(adminRole);
+//
+//		Role userRole = new Role();
+//		userRole.setName("ROLE_USER");
+//		roleRepository.save(userRole);
+//	}
 }
